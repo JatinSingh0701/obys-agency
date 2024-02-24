eca878df3c7cff895dccfbbc4f02f57bd9e6121d
Jatin Singh <jatinsingh0701@gmail.com>
2024-02-25 01:00:13 +0530

    add files and projects

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,79 +1,3 @@
-const loader = () => {
-  let tl = gsap.timeline();
-
-  // Animation for ".line h1" elements
-  tl.from(".line h1", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5,
-  });
-
-  //Animation for "#line-timer" and ".line h2 elements
-  tl.from("#line-timer", {
-    opacity: 0,
-    onStart: () => {
-      let loaderTimer = document.querySelector("#line-timer h5");
-
-      let timer = 0;
-
-      const intervalId = setInterval(() => {
-        timer++;
-        loaderTimer.textContent = timer;
-
-        if (timer === 100) clearInterval(intervalId);
-      }, 35);
-    },
-  });
-
-  // fontchange Animation
-  tl.to(".line h2", {
-    animationName: "fontchange",
-    opacity: 1,
-  });
-
-  // Animation to fade out "#loader" element
-  tl.to("#loader", {
-    opacity: 0,
-    duration: 0.4,
-    delay: 3.7, // 3.7
-  });
-
-  // Animation to bring in "#navbar" element with delay, y offset, and opacity
-  tl.from("#navbar", {
-    delay: 0.2,
-    y: 1200,
-    opacity: 0,
-    duration: 0.5,
-    ease: Power4,
-  });
-
-  // Hide the "#loader" element by setting its display to "none"
-  tl.to("#loader", {
-    display: "none",
-  });
-
-  tl.from("nav", {
-    opacity: 0,
-  });
-
-  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero4 h1", {
-    y: 160,
-    stagger: 0.2,
-  });
-};
-
-const cursor = () => {
-  document.addEventListener("mousemove", (dets) => {
-    gsap.to("#crsr", {
-      left: dets.x,
-      top: dets.y,
-    });
-  });
-  Shery.makeMagnet("#nav-part2 h4");
-  Shery.makeMagnet("nav svg");
-};
-
 const Locomotive = () => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -113,7 +37,86 @@ const Locomotive = () => {
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
 };
+const loader = () => {
+  let tl = gsap.timeline();
 
-// Locomotive();
+  // Animation for ".line h1" elements
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+
+  //Animation for "#line-timer" and ".line h2 elements
+  tl.from("#line-timer", {
+    opacity: 0,
+    onStart: () => {
+      let loaderTimer = document.querySelector("#line-timer h5");
+
+      let timer = 0;
+
+      const intervalId = setInterval(() => {
+        timer++;
+        loaderTimer.textContent = timer;
+
+        if (timer === 100) clearInterval(intervalId);
+      }, 35);
+    },
+  });
+
+  // fontchange Animation
+  tl.to(".line h2", {
+    animationName: "fontchange",
+    opacity: 1,
+  });
+
+  // Animation to fade out "#loader" element
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 0, // 3.7
+  });
+
+  // Animation to bring in "#navbar" element with delay, y offset, and opacity
+  tl.from("#navbar", {
+    delay: 0.2,
+    y: 1200,
+    opacity: 0,
+    duration: 0.5,
+    ease: Power4,
+  });
+
+  // Hide the "#loader" element by setting its display to "none"
+  tl.to("#loader", {
+    display: "none",
+  });
+
+  tl.from("nav", {
+    opacity: 0,
+  });
+
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero4 h1", {
+    y: 160,
+    stagger: 0.2,
+  });
+
+  tl.from("#hero1,#inside-navbar", {
+    opacity: 0,
+  },"-=1.2");
+};
+
+const cursor = () => {
+  document.addEventListener("mousemove", (dets) => {
+    gsap.to("#crsr", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+  Shery.makeMagnet("#nav-part2 h4");
+  Shery.makeMagnet("nav svg");
+};
+
+Locomotive();
 loader();
 cursor();
